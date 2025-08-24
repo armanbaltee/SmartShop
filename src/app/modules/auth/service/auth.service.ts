e7d10f49b8a22b131211827e1da9b5316a2f7459
armanbaltee <armanalibaltee@gmail.com>
2025-08-24 16:58:57 +0500

    feact(add signup, service and enviroment file)

---
+++ src/app/modules/auth/service/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  signupApiCall(payLoad:any){
+    return this.http.post('signupUrl', payLoad)
+  }
+}
--- src/app/modules/auth/signup/signup.component.ts
+++ src/app/modules/auth/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,4 +10,41 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  formData!: FormGroup
+  constructor(private formBuilder: FormBuilder, private snackBar: MatSnackBar, private authService: AuthService){ 
+    this.formDatas();
+  }
+
+  formDatas(){
+    this.formData = this.formBuilder.group({
+        fname: ['', [Validators.required]],
+        lname: ['', [Validators.required]],
+        phone: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+  onSubmit(){
+    if(this.formData.invalid){
+      this.snackBar.open('Error', 'Close', {
+        duration: 3000,
+        panelClass: ['error-snackbar']
+      })
+      return
+    }
+    this.authService.signupApiCall(this.formData.value).subscribe({
+      next: (res:any)=>{
+        this.snackBar.open(`${res.message}`, 'Close', {
+        duration: 3000,
+        panelClass: ['success-snackbar']
+      })
+      },
+      error: (err)=>{
+        this.snackBar.open(`${err.message}`, 'Close', {
+        duration: 3000,
+        panelClass: ['error-snackbar']
+      })
+      }
+    })
+  }
 }
